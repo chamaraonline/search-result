868fff46a43b6bd63d4647c5c2788b7aad832467
Sahan Jayawardana <sahan@clouda.io>
2019-05-23 13:59:14 +0530

    Add actual content

--- react/BrandContent.js
+++ react/BrandContent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { compose, equals, find, prop } from 'ramda'
+import { compose, equals, find, head, prop, propEq } from 'ramda'
 import { Query } from 'react-apollo'
 import getBrandContent from './queries/getBrandContent.gql'
 import getBrandList from './queries/getBrandList.gql'
@@ -50,37 +50,27 @@ class BrandContent extends Component {
             >
               {({ data, brandContentLoading, brandContentError }) => {
                 if (brandContentLoading) return <Spinner />
-                if (brandContentError) return <p>ERROR</p>
+                if (brandContentError || !data) return <p>ERROR</p>
 
-                const brandContent = {
-                  id: '27c964bc-7639-11e9-828f-d599ef464471',
-                  brandId: '2000000',
-                  description: 'Test description',
-                  newProducts: [
-                    {
-                      productId: '24',
-                      description: 'Test product 24 description',
-                    },
-                    {
-                      productId: '17',
-                      description: 'Test product 17 description',
-                    },
-                  ],
-                  videos: [
-                    {
-                      title: 'Video 1',
-                      url:
-                        'https://player.vimeo.com/video/142175573?title=0&byline=0&portrait=0',
-                    },
-                    {
-                      title: 'Video 2',
-                      url:
-                        'https://player.vimeo.com/video/166034292?title=0&byline=0&portrait=0',
-                    },
-                  ],
+                const fields = prop('fields', head(data.documents || {}))
+                if (!fields) {
+                  return <div />
                 }
+
+                const { value: description } = find(
+                  propEq('key', 'description'),
+                  fields
+                )
+                const { value: newProducts } = find(
+                  propEq('key', 'newProducts'),
+                  fields
+                )
+                const { value: videos } = find(propEq('key', 'videos'), fields)
+
                 return (
-                  <div className={`${styles.brandGalleryContainer} bb b--muted-4`}>
+                  <div
+                    className={`${styles.brandGalleryContainer} bb b--muted-4`}
+                  >
                     <Tabs>
                       <Tab
                         label="New Products"
@@ -92,8 +82,8 @@ class BrandContent extends Component {
                             styles.brandGalleryProductShelf
                           } flex flex-row flex-wrap items-stretch bn ph1 pl9-l na4`}
                         >
-                          {brandContent.newProducts &&
-                            brandContent.newProducts.map(productItem => (
+                          {newProducts &&
+                            JSON.parse(newProducts).map(productItem => (
                               <ProductShelfContent
                                 productId={productItem.productId}
                                 key={`product-content-${productItem.productId}`}
@@ -109,9 +99,9 @@ class BrandContent extends Component {
                         onClick={() => this.handleTabChange(2)}
                       >
                         <div className={styles.brandGalleryVideos}>
-                          {brandContent.videos && (
+                          {videos && (
                             <VideoSlider
-                              videos={brandContent.videos.map(video => ({
+                              videos={JSON.parse(videos).map(video => ({
                                 videoUrl: video.url,
                                 videoTitle: video.title,
                               }))}
@@ -124,9 +114,19 @@ class BrandContent extends Component {
                         active={this.state.currentTab === 3}
                         onClick={() => this.handleTabChange(3)}
                       >
-                        <div className={`${styles.brandGalleryDescription} w-100 mh9`}>
-                          {brandContent.description && (
-                            <p className={styles.brandGalleryDescriptionTextContent}>{brandContent.description}</p>
+                        <div
+                          className={`${
+                            styles.brandGalleryDescription
+                          } w-100 mh9`}
+                        >
+                          {description && (
+                            <p
+                              className={
+                                styles.brandGalleryDescriptionTextContent
+                              }
+                            >
+                              {description}
+                            </p>
                           )}
                         </div>
                       </Tab>
